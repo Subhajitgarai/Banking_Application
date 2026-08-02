6762ccb28b4ece1df8ee9eb24515bf8668554e9c
Subhajitgarai <subhajit12345@gmail.com>
2026-08-02 20:34:31 +0530

    security config conflict commit in release

--- src/main/java/com/bank/config/SecurityConfig.java
+++ src/main/java/com/bank/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig1 {
     @Autowired
     private JwtAuthenticationEntryPoint point;
     @Autowired
