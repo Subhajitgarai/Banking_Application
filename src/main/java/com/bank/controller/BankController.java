b54e62d5791f3c056d32a4cdfee1b82cfe642012
Subhajitgarai <subhajit12345@gmail.com>
2026-08-15 11:28:29 +0530

    refactor: move @EnableMethodSecurity to central SecurityConfig

--- src/main/java/com/bank/controller/BankController.java
+++ src/main/java/com/bank/controller/BankController.java
@@ -7,14 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/bank")
-@EnableMethodSecurity(prePostEnabled = true)
 public class BankController {
     //Create bank
     @Autowired
